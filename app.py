dc0da5f8e0b8021a93afee26c31a1a28460289ea
Naresh Babral <naresh.babral@innovaccer.com>
2016-02-10 21:18:37 +0530

    Made changes to return types first stable Backend API

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask, flash, redirect, url_for, request, get_flashed_messages
+from flask import Flask, flash, redirect, url_for, request, get_flashed_messages, jsonify
 from pymongo import MongoClient
 import json
 from bson import json_util
 import pycurl, json
 from flask.ext.cors import CORS, cross_origin
 import cStringIO
+import ast
+import requests
 
 app = Flask(__name__)
 cors = CORS(app)
@@ -26,25 +28,28 @@ def index():
         '''
     )
 
+def Mapping(col):
+	return ast.literal_eval(col)
 
 @app.route('/getTablesInfo')
 @cross_origin()
 def getTablesInfo():
-    cursor = db.tables.find()
+    cursor = db.tables.find({},{'_id':0})
     json_docs = []
     for doc in cursor:
-        json_str = json.dumps(doc, default=json_util.default)
-        json_docs.append(json_str)
-    return json.dumps(json_docs)
+		doc['columns'] = map( Mapping, doc['columns'])
+		json_docs.append(doc)
+    return jsonify({'data': json_docs})
 
 @app.route('/postJob', methods = ['POST'])
 @cross_origin()
 def runSparkJob():
 	payload = request.get_json()
 	jobResponse = cStringIO.StringIO()
+	print json.dumps(payload)
 	curlreq = pycurl.Curl()
 	print json.dumps(payload)
-	url = 'http://172.16.248.156:8090/jobs?appName=final&classPath=DataChecks.basicStats'
+	url = 'http://172.16.248.156:8090/jobs?appName=check&classPath=DataChecks.basicStats'
 	curlreq.setopt(pycurl.URL, url)
 	curlreq.setopt(pycurl.HTTPHEADER, ['Accept: application/json'])
 	curlreq.setopt(pycurl.POST, 1)
@@ -56,7 +61,6 @@ def runSparkJob():
 	js['response'] = json.loads(jobResponse.getvalue())
 	js['jobId'] = json.loads(jobResponse.getvalue())['result']['jobId']
 	db.configs.save(js)
-	print jobResponse.getvalue()
 	return jobResponse.getvalue()
 
 @app.route('/getResults', methods = ['POST'])
@@ -64,15 +68,11 @@ def runSparkJob():
 def getResults():
 	jobResponse = cStringIO.StringIO()
 	jobIds = request.get_json()
+	print jobIds
 	for jobId in jobIds:
 		url = 'http://172.16.248.156:8090/jobs/' + jobId
-		curlreq = pycurl.Curl()
-		curlreq.setopt(pycurl.URL, url)
-#		curlreq.setopt(pycurl.HTTPHEADER, ['Accept: application/json'])
-		curlreq.setopt(curlreq.WRITEFUNCTION, jobResponse.write)
-		curlreq.perform()
-		response = json.loads(jobResponse.getvalue())
-		curlreq.close()
+		r = requests.get(url)
+		response = json.loads(r.text)
 		print response['status']
 		if response['status'] == "FINISHED" or response['status'] == "ERROR":
 			db.configs.update({'jobId':jobId}, {'$set': {'response': response}})
@@ -82,12 +82,12 @@ def getResults():
 @app.route('/listConfigs')
 @cross_origin()
 def listConfigs():
-	cursor = db.configs.find()
+	cursor = db.configs.find({},{'_id':0})
 	json_docs = []
 	for doc in cursor:
-		json_str = json.dumps(doc, default=json_util.default)
-		json_docs.append(json_str)
-	return json.dumps(json_docs)
+		json_docs.append(doc)
+	print json.dumps(json_docs)
+	return jsonify({'data': json_docs})
 
 #@app.route('/addConfigs', methods = ['POST'])
 #@cross_origin()
@@ -100,18 +100,17 @@ def listConfigs():
 @cross_origin()
 def getConfig():
 	configName = request.get_json()
-	cursor = db.configs.find({'configName': configName})
+	cursor = db.configs.find({'configName': configName}, {'_id':0})
 	json_docs = []
 	for doc in cursor:
-		json_str = json.dumps(doc, default=json_util.default)
-		json_docs.append(json_str)
-	return json.dumps(json_docs)
+		json_docs.append(doc)
+	return jsonify({'data': json_docs})
 
-#@app.route('/saveResults')
-#@cross_origin()
-#def saveResults():
-#	return "True"
+@app.route('/reRunJob', methods = ['POST'])
+@cross_origin()
+def saveResults():
+	return "True"
 
 
 if __name__ == '__main__':
-    app.run(host = "0.0.0.0", port = 9999, debug = True)
+    app.run(host = "0.0.0.0", port = 5111, debug = True)
